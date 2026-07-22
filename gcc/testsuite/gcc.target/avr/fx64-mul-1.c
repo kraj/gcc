65b50ebf4bbdf39388cd83030d9441111663604b
Georg-Johann Lay <avr@gjlay.de>
2026-07-22 12:25:30 +0200

    AVR: Tweak lib1funcs-fixed.S::__mulQ64_work.
    This patch
    - Tweaks the signed overflow handling in __mulQ64_work.
    - Rename misnomed fx24-mul.c to fx64-mul-1.c.
    - Adds more test cases in fx64-mul-2.c.
    
    libgcc/
    	* config/avr/lib1funcs-fixed.S (__mulQ64_work): Tweak
    	overflow handling.  Use __negsi2 (non-saturating negation)
    	to negate.
    gcc/testsuite/
    	* gcc.target/avr/fx24-mul.c: Renamed to...
    	* gcc.target/avr/fx64-mul-1.c: ...this.
    	* gcc.target/avr/fx64-mul-2.c: New test.
    	* gcc.target/avr/fx64-mul.h: New file.

--- gcc/testsuite/gcc.target/avr/fx64-mul-1.c
+++ gcc/testsuite/gcc.target/avr/fx64-mul-1.c
@@ -3,63 +3,7 @@
 
 // !!! Requires the fx64 <-> float conversions from AVR-LibC.
 
-#include <stdfix.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <avr/pgmspace.h>
-
-#define NI __attribute((noipa))
-
-typedef long accum lk_t;
-typedef long long accum llk_t;
-typedef long long fract llr_t;
-
-typedef unsigned long accum ulk_t;
-typedef unsigned long long accum ullk_t;
-typedef unsigned long long fract ullr_t;
-
-// Values are in fmin <= x < fmax.
-#define fmax_llr 1.0f
-#define fmin_llr (-fmax_llr)
-#define fmax_ullr fmax_llr
-#define fmin_ullr 0.0f
-
-#define fmax_lk 0x1.0p32f
-#define fmin_lk (-fmax_lk)
-#define fmax_ulk fmax_lk
-#define fmin_ulk 0.0f
-
-#define fmax_llk 0x1.0p16f
-#define fmin_llk (-fmax_llk)
-#define fmax_ullk fmax_llk
-#define fmin_ullk 0.0f
-
-#define UMAX 0xffffffffffffffff
-#define SMAX 0x7fffffffffffffff
-#define SMIN 0x8000000000000000
-
-// Values are in min <= x <= max.
-#define max_lk lkbits (SMAX)
-#define min_lk lkbits (SMIN)
-#define max_ulk ulkbits (UMAX)
-#define min_ulk ulkbits (0)
-
-#define max_llk llkbits (SMAX)
-#define min_llk llkbits (SMIN)
-#define max_ullk ullkbits (UMAX)
-#define min_ullk ullkbits (0)
-
-#define max_llr llrbits (SMAX)
-#define min_llr llrbits (SMIN)
-#define max_ullr ullrbits (UMAX)
-#define min_ullr ullrbits (0)
-
-#define id_lk   10
-#define id_ulk  20
-#define id_llk  30
-#define id_ullk 40
-#define id_llr  50
-#define id_ullr 60
+#include "fx64-mul.h"
 
 #define MK_TEST(fx)				    \
   NI bool in_range_##fx (float x)		    \
@@ -110,8 +54,6 @@ NI void test_mul (float a, float b)
   test_mul_llr (a, b);
   test_mul_ullr (a, b);
 }
-
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
 
 // Results / arguments must be representable as float, so no rounding occurs.
 // No-overflow results must be representable as fixed, so no rounding occurs.
